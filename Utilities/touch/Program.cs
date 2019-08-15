87ca9b4b517ad56c529192da6412bff0d581f80e
Jason Lu <jasonluX13@gmail.com>
2019-08-15 13:56:10 -0400

    Added ls and touch functionality

---
+++ Utilities/touch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace touch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string filename = args[0];
+                if (File.Exists(filename))
+                {
+                    //Update last modified time
+                    File.SetLastAccessTime(filename, DateTime.Now);
+                }
+                else
+                {
+                    //Create new file
+                    File.Create(filename);
+                }
+            }
+        }
+    }
+}
