f9d0e6ca59abd977551fe584830239864b27bb4a
Jason Lu <jasonluX13@gmail.com>
2019-08-22 12:35:07 -0400

    Updated error handling with 404 handling

--- ErrorHandlingExample/ErrorHandlingExample/Global.asax.cs
+++ ErrorHandlingExample/ErrorHandlingExample/Global.asax.cs
@@ -12,5 +12,21 @@ namespace ErrorHandlingExample
         protected void Application_Start(object sender, EventArgs e)
         {
         }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Get the most recent error in the application,
+            // the error that caused this handler to run.
+            Exception exc = Server.GetLastError();
+
+            // If the error is an unhandled exception that
+            // Web Forms is propagating through its error
+            // handlers...
+            if (exc is HttpUnhandledException)
+            {
+                // Then, redirect the request to ServerError.aspx.
+                Server.Transfer("MissingPage.aspx");
+            }
+        }
     }
 }
---
+++ ErrorHandlingExample/ErrorHandlingExample/MissingPage.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MissingPage.aspx.cs" Inherits="ErrorHandlingExample.MissingPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>The page you are looking for does not exist.</h1>
+    </div>
+    </form>
+</body>
+</html>
