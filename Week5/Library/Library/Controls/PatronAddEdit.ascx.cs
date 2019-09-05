54e29b9ead20f5bc2afdd93b22706d3096b3bb5f
Jason Lu <jasonluX13@gmail.com>
2019-09-05 08:56:14 -0400

    Further restricted access for Patrons

--- Week5/Library/Library/Controls/PatronAddEdit.ascx.cs
+++ Week5/Library/Library/Controls/PatronAddEdit.ascx.cs
@@ -10,7 +10,7 @@ using System.Web.UI.WebControls;
 
 namespace Library.Controls
 {
-    public partial class PatronAddEdit : System.Web.UI.UserControl
+    public partial class PatronAddEdit : BaseControl
     {
         int patronId = 0;
         public bool edit { get; set; }
