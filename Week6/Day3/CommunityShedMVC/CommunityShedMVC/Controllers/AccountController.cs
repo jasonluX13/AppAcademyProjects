c0b1d097ff0146b483ae39b6e1f4ea7c782f80bc
Jason Lu <jasonluX13@gmail.com>
2019-09-11 14:31:47 -0400

    Logout

--- Week6/Day3/CommunityShedMVC/CommunityShedMVC/Controllers/AccountController.cs
+++ Week6/Day3/CommunityShedMVC/CommunityShedMVC/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace CommunityShedMVC.Controllers
 {
@@ -21,8 +22,19 @@ namespace CommunityShedMVC.Controllers
         [AllowAnonymous]
         public ActionResult Login(LoginViewModel viewModel)
         {
-
+            if (ModelState.IsValid)
+            {
+                FormsAuthentication.SetAuthCookie(viewModel.Email, false);
+                return RedirectToAction("Index", "Home");
+            }
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
         }
     }
 }
