fc4cc368aa2d9d763f9bc73a7595684aea658f38
Jason Lu <jasonluX13@gmail.com>
2019-09-11 12:39:30 -0400

    Locked down homepage and installed bcrypt library

---
+++ Week6/Day3/CommunityShedMVC/CommunityShedMVC/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using CommunityShedMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommunityShedMVC.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel viewModel)
+        {
+
+            return View(viewModel);
+        }
+    }
+}
